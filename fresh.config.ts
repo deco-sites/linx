2705d9174933b6b331a17e1036685cdaebdc9d81
vitoo <victorhugomeurerdelgrandi@gmail.com>
2024-01-22 08:06:17 -0400

    feat: install islands plugin

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "$fresh/server.ts";
 import plugins from "https://denopkg.com/deco-sites/std@1.22.0/plugins/mod.ts";
 import manifest from "./manifest.gen.ts";
+import { islands } from "$plugins/mod.ts";
 
 export default defineConfig({
-  plugins: plugins({ manifest }),
+  plugins: [...plugins({ manifest }), islands()],
 });
